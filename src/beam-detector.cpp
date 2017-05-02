d1a0bf05b69dc363bfce018b7eeba89eec4f837e
Julien Vanier <julien@particle.io>
2017-05-02 00:28:24 -0400

    Add way to turn power on and off

--- src/beam-detector.cpp
+++ src/beam-detector.cpp
@@ -4,24 +4,45 @@
 #include "beam-detector.h"
 
 BeamDetector::BeamDetector(pin_t lightPin, pin_t powerPin) :
+  enabled(false),
   lightPin(lightPin),
   powerPin(powerPin),
   debounceTime(DEBOUNCE_TIME)
 {
 }
 
-void BeamDetector::begin()
+void BeamDetector::begin(bool enablePower)
 {
   // Setup pins
   pinMode(powerPin, OUTPUT);
-  digitalWrite(powerPin, HIGH);
   pinMode(lightPin, INPUT);
+
+  // Turn on power if required
+  enable(enablePower);
 
   // Setup interrupt
   attachInterrupt(lightPin, &BeamDetector::interruptHandler, this, DETECTION_EDGE);
 }
 
+void BeamDetector::enable(bool enablePower) {
+  if (enablePower != enabled) {
+    auto now = millis();
+    {
+      AtomicSection atomic; // this block cannot be interrupted
+
+      // set a lastToggleTime in the future to avoid false interrupts
+      // when turning the power on and off
+      lastToggleTime = now + POWER_ON_DEBOUNCE_TIME;
+      enabled = enablePower;
+    }
+    digitalWrite(powerPin, enabled ? POWER_ON : POWER_OFF);
+  }
+}
+
 bool BeamDetector::beamBroken() {
+  if (!enabled) {
+    return false;
+  }
   return digitalRead(lightPin) == BEAM_BROKEN;
 }
 
@@ -34,6 +55,9 @@ void BeamDetector::setDebounceTime(uint16_t ms) {
 }
 
 void BeamDetector::interruptHandler() {
+  if (!enabled) {
+    return;
+  }
   // Debounce the edge transistion to avoid counting multiple detections
   // when the light level is close to the transition point
   auto now = millis();
--- src/beam-detector.h
+++ src/beam-detector.h
@@ -9,13 +9,17 @@ class BeamDetector
 {
 public:
   static constexpr auto DEBOUNCE_TIME = 5;
+  static constexpr auto POWER_ON_DEBOUNCE_TIME = 20;
   static constexpr auto POWER_ON = HIGH;
+  static constexpr auto POWER_OFF = LOW;
   static constexpr auto BEAM_BROKEN = LOW;
   static constexpr auto DETECTION_EDGE = FALLING;
 
   BeamDetector(pin_t lightPin, pin_t powerPin);
 
-  void begin();
+  void begin(bool enablePower = true);
+
+  void enable(bool enablePower);
 
   bool beamBroken();
   uint32_t detectionCount();
@@ -26,6 +30,7 @@ private:
   void interruptHandler();
 
 private:
+  bool enabled;
   pin_t lightPin;
   pin_t powerPin;
   uint16_t debounceTime;
